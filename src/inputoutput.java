56217e154f332349fb88e3e992ff3fb6c7f8a795
AlfieMcPhee <mcpheeDev@proton.me>
2026-06-17 12:57:24 +0100

    Added more input depending on type

--- src/inputoutput.java
+++ src/inputoutput.java
@@ -1,15 +1,21 @@
 import java.util.Scanner;
 
-
-
 public class inputoutput {
     public static void main(String[] args){
 
-
         Scanner scanner = new Scanner(System.in);
+
         System.out.print("Enter your name ");
         String name = scanner.nextLine(); // Assigning variable
         System.out.println("Your name is " + name);
+
+        System.out.print("Enter your age ");
+        int age = scanner.nextInt(); // Integers
+        System.out.println("You are " + age + " Years old");
+
+        System.out.println("Enter your GPA");
+        double gpa = scanner.nextDouble(); // Doubles
+        System.out.println("Your GPA is " + gpa);
 
 
 
