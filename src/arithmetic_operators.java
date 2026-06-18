2831045ccdf02b4a9942e904c80b5279f6869464
Alfie McPhee <mcphee.dev@proton.me>
2026-06-18 08:22:01 +0100

    Learning arithmetic operators

---
+++ src/arithmetic_operators.java
@@ -0,0 +1,12 @@
+public class arithmetic_operators {
+    public static void main(String[] args){
+        // Arithmetic Operators
+        int x = 10;
+        int y = 2;
+        int z;
+
+        z = x + y;
+        System.out.println(z);
+
+    }
+}
